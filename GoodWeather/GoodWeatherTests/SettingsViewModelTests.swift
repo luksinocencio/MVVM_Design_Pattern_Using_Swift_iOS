d0bea9809e02d3601479c384ee839d8f01acb40a
p21lucasi <lucas.inocencio@p21sistemas.com.br>
2020-03-06 18:03:08 -0300

    -

---
+++ GoodWeather/GoodWeatherTests/SettingsViewModelTests.swift
@@ -0,0 +1,44 @@
+//
+//  SettingsViewModelTests.swift
+//  GoodWeatherTests
+//
+//  Created by P21 Sistemas on 06/03/20.
+//  Copyright © 2020 Lucas Inocencio. All rights reserved.
+//
+
+import XCTest
+@testable import GoodWeather
+
+class SettingsViewModelTests: XCTestCase {
+    
+    private var settingsVM: SettingsViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        
+        self.settingsVM = SettingsViewModel()
+    }
+    
+    func test_default_value_celsius() {
+        XCTAssertEqual(settingsVM.selectedUnit, .celsius)
+    }
+    
+    func test_celsius() {
+        XCTAssertEqual(self.settingsVM.selectedUnit.displayName, "Celsius")
+    }
+    
+    
+    func test_selection() {
+        self.settingsVM.selectedUnit = .fahrenheit
+        let userDefaults = UserDefaults.standard
+        XCTAssertNotNil(userDefaults.value(forKey: "unit"))
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        let userDefaults = UserDefaults.standard
+        userDefaults.removeObject(forKey: "unit")
+    }
+    
+}
